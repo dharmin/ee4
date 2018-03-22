f3701104e7ff6dae50615ca7ed728134191c0ae2
Dharmin Shah <castiel.shah@gmail.com>
2018-03-22 21:38:14 +0530

    Add ee4 shell script

--- Dockerfiles/base-dock/version/debian-stretch-slim/entrypoint.sh
+++ Dockerfiles/base-dock/version/debian-stretch-slim/entrypoint.sh
@@ -9,11 +9,13 @@ groupadd -r -g $G_ID ee
 useradd -r -d $EE_HOME -u $U_ID -g ee ee
 addgroup ee docker
 
-chown -R ee:ee $EE_HOME
-mkdir -p $EE_HOME/.ee4 $EE_HOME/Sites
-chown -R ee:ee $EE_HOME
+# mkdir -p $EE_HOME/.ee4 $EE_HOME/Sites
+# chown -R ee:ee $EE_HOME
 
-cat > /tmp/entrypoint.sh << EOF
-#!/bin/sh
-tail -f /dev/null
-EOF
+CMND="$@"
+
+docker run --net=host -t --rm --dns 172.17.0.2 -e ARGS="$CMND" -e HOST_HOME=$HOST_HOME -e USER_ID=$USER_ID -e GROUP_ID=$GROUP_ID -e  DOCKER_ID=$DOCKER_ID -v /var/run/docker.sock:/var/run/docker.sock -v $HOST_HOME/.ee4:$HOST_HOME/.ee4 -v $HOST_HOME/Sites:$HOST_HOME/Sites  dharmin/ee4
+# cat > /tmp/entrypoint.sh << EOF
+# #!/bin/sh
+# tail -f /dev/null
+# EOF
--- Dockerfiles/ee4-dock/version/debian-stretch/entrypoint.sh
+++ Dockerfiles/ee4-dock/version/debian-stretch/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 EE_HOME=${HOST_HOME}
-
 U_ID=${USER_ID}
 G_ID=${GROUP_ID}
 D_ID=${DOCKER_ID}
 
-groupadd -r -g $D_ID docker
-groupadd -r -g $G_ID ee && useradd -r -d $EE_HOME -u $U_ID -g ee ee
+groupadd -g $D_ID docker
+useradd -u $U_ID ee
+groupmod -g $G_ID ee
+usermod -d $EE_HOME -u $U_ID ee
 addgroup ee docker
 
-gosu ee /usr/local/bin/ee4 --debug $@
-/bin/bash
+gosu ee /usr/local/bin/ee4 ${ARGS}
